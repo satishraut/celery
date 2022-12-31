67749696b3c385fa6c7310c8e785c24b5cf0dd5b
SatishRaut121 <satish.raut@adcreators.com.au>
2022-12-31 14:21:18 +0530

    minor

--- celery_example/celery.py
+++ celery_example/celery.py
@@ -18,7 +18,7 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 # Load task modules from all registered Django apps.
 app.conf.beat_schedule = {
     'every-15-seconds':{
-        'task':'notifications.tasks.send_email',
+        'task':'notifications.tasks.send_mail',
         'schedule':15,
         'args':('satish.raut777@gmail.com',)
     }
